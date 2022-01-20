f8fb9692348f1829fec34a165b789b96327b09bc
st1chka <kalinine03@gmail.com>
2022-01-20 22:10:43 +0300

    add time

--- src/main/java/TelegramBotMain.java
+++ src/main/java/TelegramBotMain.java
@@ -4,23 +4,31 @@ import lombok.SneakyThrows;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.util.Objects;
-import java.util.Properties;
+
+import java.util.*;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class TelegramBotMain extends TelegramLongPollingBot {
+
+
+    long durationInMillis = System.currentTimeMillis();
+    long millis = durationInMillis % 1000;
+    long second = (durationInMillis / 1000) % 60;
+    long minute = (durationInMillis / (1000 * 60)) % 60;
+    long hour = ((durationInMillis / (1000 * 60 * 60)) % 24) + 3;
+
 
     private final String botUserName;
     private final String botToken;
 
     @SneakyThrows
     TelegramBotMain() {
-//        String rootPath = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("")).getPath();
-        File appConfigPath = new File( "src/main/resources/app.properties");
+        File appConfigPath = new File("src/main/resources/app.properties");
         Properties properties = new Properties();
         properties.load(new FileInputStream(appConfigPath));
         botUserName = properties.getProperty("botUserName");
@@ -30,6 +38,7 @@ public class TelegramBotMain extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
+
         return botUserName;
     }
 
@@ -38,29 +47,29 @@ public class TelegramBotMain extends TelegramLongPollingBot {
         return botToken;
     }
 
+    private String chat_id;
+
+    public String getChat_id() {
+        return chat_id;
+    }
+
 
     @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
+        chat_id = String.valueOf(update.getMessage().getChatId());
+
+
         if (update.hasMessage() && update.getMessage().hasText()) {
-
-            SendMessage sendMessage = new SendMessage();
-
-            String message_text = update.getMessage().getText();
-            String chat_id = String.valueOf(update.getMessage().getChatId());
-
-            switch (message_text) {
-                default:{
-
-                    execute(SendMessage.builder()
-                            .chatId(chat_id)
-                            .text(update.getMessage().getText())
-                            .build());
-                    break;
-                }
-
-
+            if (String.valueOf(minute).equals("10")){
+                execute(SendMessage.builder()
+                        .chatId(chat_id)
+                        .text((hour + ":" + minute))
+                        .build());
             }
+
         }
+
     }
+
 }
